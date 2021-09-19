ff96afed0655ae7f22f09158685d7b78bcffcb99
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-09-20 02:17:57 +0530

    Added GET and POST

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res
+//         .status(200)
+//         .json({ message: 'Hello from the Server Side!', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: "This Tour doesn't exist in database!",
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'Success',
+    results: tour.length,
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}`);
+});
