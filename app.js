d0d09e0ae1dfd6945ea9ca73e852ed8f40d97b09
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-09-20 02:33:07 +0530

    PATCH Request

--- app.js
+++ app.js
@@ -27,19 +27,35 @@ app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
 
   const id = req.params.id * 1;
-  if (id > tours.length) {
+  const tour = tours.find((el) => el.id === id);
+  //   if (id > tours.length) {
+  if (!tour) {
     return res.status(400).json({
       status: 'Fail',
       message: "This Tour doesn't exist in database!",
     });
   }
-  const tour = tours.find((el) => el.id === id);
-
   res.status(200).json({
     status: 'Success',
     results: tour.length,
     data: {
       tour: tour,
+    },
+  });
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: "This Tour doesn't exist in database!",
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: '<Updated tour here...>',
     },
   });
 });
