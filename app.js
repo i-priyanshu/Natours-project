33bc4718e34233eafbf6067fb6314f94c9cab8da
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-09-22 01:15:59 +0530

    Added Seprate Routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const res = require('express/lib/response');
-const fs = require('fs');
+const tourRouter = require('./routes/tourRouter.js');
+const userRouter = require('./routes/userRoutes.js');
 const morgan = require('morgan');
 const app = express();
 
-//MIDDLEWARES
+//1) MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -13,138 +13,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// 2)  ROUTES
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
 
-// ROUTE HANDLERS:
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'Success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  //   if (id > tours.length) {
-  if (!tour) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: "This Tour doesn't exist in database!",
-    });
-  }
-  res.status(200).json({
-    status: 'Success',
-    results: tour.length,
-    data: {
-      tour,
-    },
-  });
-};
-
-const EditTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: "This Tour doesn't exist in database!",
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      message: '<Updated tour here...>',
-    },
-  });
-};
-
-const DeleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: "This Tour doesn't exist in database!",
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const CreateTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const getAllUsers = () => {
-  res.json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const createUser = () => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-const getUser = () => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-const deleteUser = () => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const updateUser = () => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', CreateTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', EditTour);
-// app.delete('/api/v1/tours/:id', DeleteTour);
-
-app.route('/api/v1/tours').get(getAllTours).post(CreateTour);
-
-app.route('/api/v1/tours/:id').get(getTour).patch(EditTour).delete(DeleteTour);
-
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app.route('/api/v1/users/:id').get(getUser).patch(updateUser).post(deleteUser);
-
+// 3) START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
