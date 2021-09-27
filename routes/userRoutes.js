924f9b869d1d3711f42fa07007533f118498daea
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-09-27 23:44:40 +0530

    DATABASE

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const userController = require('../controllers/userController.js');
-const router = express.Router();
 
-router
-  .route('/')
+const Router = express.Router();
+
+const userController = require('../controllers/userController.js');
+
+Router.route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
-router
-  .route('/:id')
+Router.route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .post(userController.deleteUser);
 
-module.exports = router;
+module.exports = Router;
