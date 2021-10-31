ff3f19fcc1615b2b7ccab575627d84db38b67a08
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-10-31 14:43:40 +0530

    Updated The TourModel

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,7 +50,7 @@ exports.CreateTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
