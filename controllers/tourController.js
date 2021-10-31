a08625df1185851ec937ac2c21d1109dc265ae99
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-10-31 14:19:55 +0530

    Added CRUD Mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,18 +55,38 @@ exports.CreateTour = async (req, res) => {
   }
 };
 
-exports.EditTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      message: '<Updated tour here...>',
-    },
-  });
+exports.EditTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.DeleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.DeleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
