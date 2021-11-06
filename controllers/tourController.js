fed1b0c15ba19d862056fd3e5e79e6043580c22f
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-11-07 01:52:11 +0530

    Added monthly-plan router and its pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -141,3 +141,53 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.monthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }, // "$push" -  creates an array in which data can be pushed...
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0 }, // sets the visibility of any property, 0 to not show and 1 for showing them
+      },
+      {
+        $sort: { numTourStarts: -1 }, // 1 for ascending and -1 for descending
+      },
+      {
+        $limit: 12, // no. of documents we want to show
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
