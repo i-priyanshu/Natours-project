4b66b01e3375bc3ca40366d5f14609c8ff4e5754
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-11-18 00:38:06 +0530

    Added Else Route Handler

--- app.js
+++ app.js
@@ -13,12 +13,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello form the Middleware!');
+  req.requestTime = new Date().toISOString();
   next();
 });
 
 // 2)  ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+// Exception Route Handler
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find the ${req.url} on this server!`,
+  });
+});
 
 module.exports = app;
