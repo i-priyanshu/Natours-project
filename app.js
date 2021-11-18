84b52ed2853db4caa0d6c4a6439e467b4f26d2da
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-11-18 23:43:14 +0530

    Added Error Handler

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter.js');
 const userRouter = require('./routes/userRoutes.js');
 const morgan = require('morgan');
@@ -28,21 +31,13 @@ app.all('*', (req, res, next) => {
   //   message: `Can't find the ${req.url} on this server!`,
   // });
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
