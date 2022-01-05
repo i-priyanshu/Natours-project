f85a3f4a972d52ea22c7bed5826b665d41ad63e8
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-05 20:20:19 +0530

    Protecting Routes - 1

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tourController = require('../controllers/tourController.js');
-
+const authController = require('../controllers/authController');
 const router = express.Router();
 // router.param('id', tourController.checkId);
 
@@ -13,7 +13,7 @@ router.route('/monthly-plan/:year').get(tourController.monthlyPlan);
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.CreateTour);
 
 router
