f8599a067719fdeacb8e5a2e16f44065f4822404
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-05 21:59:24 +0530

    Pretecting Routes - 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,15 +75,20 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists.
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError('The user belonging to this token no longer Exists!', 401)
     );
   }
 
   // 4) Check if user changed password after the token was issued.
-  freshUser.changedPasswordAfter(decoded.iat);
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please login again.', 401)
+    );
+  }
 
+  req.user = currentUser;
   next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -62,9 +62,16 @@ userSchema.methods.correctPassword = async function (
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
-    console.log(this.passwordChangedAt, JWTTimestamp);
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    // console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
   }
 
+  // GRANT ACCESS TO PROTECTED ROUTES.
   return false;
 };
 
