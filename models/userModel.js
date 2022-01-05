d4c3468f7b37993a56ccbd5cf15050f1ac7bdb77
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-05 21:09:51 +0530

    Authorization edge case handling

--- models/userModel.js
+++ models/userModel.js
@@ -36,6 +36,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -56,6 +57,14 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    console.log(this.passwordChangedAt, JWTTimestamp);
+  }
+
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
