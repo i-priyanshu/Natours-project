c04e116e5c8b8e6aa4908d8028887db6ffa4a523
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-09 21:08:16 +0530

    Implementing Request Limiter

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,11 +8,18 @@ const userRouter = require('./routes/userRoutes.js');
 const morgan = require('morgan');
 const app = express();
 
-//1) MIDDLEWARES
+//1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an Hour!',
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
