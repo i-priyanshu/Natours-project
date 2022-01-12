d78fc4e71958392978d83742eb8ee35eb2fd6b4b
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-12 19:53:38 +0530

    Embedding Users in TourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const User = require('./userModel');
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -103,6 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -112,6 +113,13 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises); // VID-5 ,SEC - 11
+  next();
 });
 
 // QUERY MIDDLEWARE
