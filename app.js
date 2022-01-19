5fb251b7a1a958addf8ab5e817666cafae4636de
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2022-01-20 03:03:33 +0530

    Added PUG template Engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -13,8 +14,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 const morgan = require('morgan');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //1) GLOBAL MIDDLEWARES
 app.use(helmet());
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -52,9 +58,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -63,6 +66,10 @@ app.use((req, res, next) => {
 });
 
 // 2)  ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
